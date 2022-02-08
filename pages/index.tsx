ccc8bb2cdb3d1aa276c482fd083ecfe5ac0a98ed
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2022-02-08 16:59:02 +0200

    Add Button primary | ghost

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,10 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">First Page</Htag>
-      <Button appereance="primary">Узнать побробнее</Button>
-      <Button appereance="ghost" arrow="down">
+      <Button appereance="primary" arrow="right">
+        Узнать побробнее
+      </Button>
+      <Button appereance="ghost" arrow="right">
         Узнать побробнее
       </Button>
     </>
