e3b864150f5511261045e5dbfd253b18e073e275
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2022-03-02 18:04:01 +0200

    Create Grid Template

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,19 +3,19 @@ import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
 import { LayoutProps } from './LayoutProps';
-import cn from 'classnames';
 import styles from './Layout.module.css';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        {children}
+
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>
+       {children}
       </div>
-      <Footer />
-    </>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
