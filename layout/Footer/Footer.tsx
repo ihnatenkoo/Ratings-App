42c10ca70eb07d52ea31f36506032046b5473103
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2022-03-03 14:02:33 +0200

    Footer Grid + Adaptive

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,20 @@
 import { FooterProps } from './Footer.props';
 import cn from 'classnames';
 import styles from './Footer.module.css';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer {...props} className={cn(className, styles.footer)}>
+      <span className="copyright">
+        OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+      </span>
+      <a href="#" target="_blank" className="terms">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank" className="confidentiality">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
